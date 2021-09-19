baa9d5835add85dd7d9a3bfd9e796e2b742b0d41
Ankan Mondal <ankanmondal45@gmail.com>
2021-09-19 21:08:01 +0530

    Added menu image Added 5 icons, background and a clock Added close and done icons Added time text view Added Set Alarm button Added label text

--- app/src/main/java/com/example/smartalarm/AddAlarm_Activity.java
+++ app/src/main/java/com/example/smartalarm/AddAlarm_Activity.java
@@ -1,19 +1,49 @@
 package com.example.smartalarm;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.NoCopySpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
-public class AddAlarm_Activity extends AppCompatActivity {
+public class AddAlarm_Activity extends AppCompatActivity implements TimePickerDialog {
+    private TextView alarm_time;
+    private Button cancel_alarm;
+    private Button set_alarm;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addalarm_activity);
+        alarm_time = findViewById(R.id.alarm_time);
         Intent intent = getIntent();
         setTitle("Add alarm");
+        Button set_alarm = findViewById(R.id.set_alarm);
+        set_alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+        cancel_alarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cancelAlarm();
+            }
+        });
     }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+    public void onTimeSet() {}
 }
