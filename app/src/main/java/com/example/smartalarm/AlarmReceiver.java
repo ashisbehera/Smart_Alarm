977caca5cb87378d311b5a3a52aa390d26165b69
Ankan Mondal <ankanmondal45@gmail.com>
2021-09-22 21:00:27 +0530

    Added menu image Added 5 icons, background and a clock Added close and done icons Added time text view Added Set Alarm button Added label text Added hour and minute text view Set label opacity to 30% Create AlarmReceiver class Tried a bunch of stuffs in AddAlarm_Activity Reworked on AlarmReceiver.java

--- app/src/main/java/com/example/smartalarm/AlarmReceiver.java
+++ app/src/main/java/com/example/smartalarm/AlarmReceiver.java
@@ -1,37 +1,29 @@
 package com.example.smartalarm;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Vibrator;
-import android.widget.Toast;
 
-import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class AlarmReceiver extends BroadcastReceiver {
-    @RequiresApi(api = Build.VERSION_CODES.Q)
+
     @Override
-    // implement onReceive() method
     public void onReceive(Context context, Intent intent) {
-
-
-
-
-
-        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
-        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        if (alarmUri == null) {
-            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        }
-
-        // setting default ringtone
-        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
-
-        // play ringtone
-        ringtone.play();
+        Intent i = new Intent(context, AlarmActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, i, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notification_alarm")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Smart Alarm Manager")
+                .setContentText("Notification")
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(1, builder.build());
     }
 }
