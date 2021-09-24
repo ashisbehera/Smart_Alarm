7e8973b4b16ec2a714e1282e87357c500450f9e5
Ankan Mondal <ankanmondal45@gmail.com>
2021-09-24 18:26:42 +0530

    Update AddAlarm_Activity.java

--- app/src/main/java/com/example/smartalarm/AddAlarm_Activity.java
+++ app/src/main/java/com/example/smartalarm/AddAlarm_Activity.java
@@ -1,9 +1,12 @@
 package com.example.smartalarm;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.AlarmClock;
 import android.text.NoCopySpan;
@@ -17,32 +20,32 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.example.smartalarm.databinding.ActivityMainBinding;
 
 import java.util.Calendar;
 
 public class AddAlarm_Activity extends AppCompatActivity {
     private TimePicker timePicker;
     private Button cancel_alarm;
-    private FloatingActionButton set_alarm;
+    private Button set_alarm;
     private AlarmConstraints newAlarm;
-    private StringBuilder timeBuilder=new StringBuilder();
-
+    private StringBuilder timeBuilder = new StringBuilder();
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addalarm_activity);
+        createNotificationChannel();
         setTitle("Add alarm");
         /**
          *set alarm button
          */
-        set_alarm = (FloatingActionButton) findViewById(R.id.set_alarm_fbt);
+        set_alarm = findViewById(R.id.set_alarm);
         /**
          *time picker
          */
-        timePicker=(TimePicker) findViewById(R.id.timePicker);
-        /**
+        timePicker = (TimePicker) findViewById(R.id.timePicker);/**
          *initialing the alarmcontraints button
          */
         newAlarm = new AlarmConstraints();
@@ -63,22 +66,30 @@ public class AddAlarm_Activity extends AppCompatActivity {
     }
 
     /**
-     *collect the time from the time picker
+     * collect the time from the time picker
      */
-    private String getpickerTime()
-    {
+    private String getpickerTime() {
         timeBuilder.append(String.valueOf(timePicker.getCurrentHour()));
         timeBuilder.append(":");
-        String minute=timePicker.getCurrentMinute().toString();
-        if(minute.length()==1)
-        {
+        String minute = timePicker.getCurrentMinute().toString();
+        if (minute.length() == 1) {
             timeBuilder.append("0").append(minute);
-        }
-        else
-        {
+        } else {
             timeBuilder.append(minute);
         }
 
         return timeBuilder.toString();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Reminder channel";
+            String description = "Channel for alarm manager";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("notification_alarm", name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
