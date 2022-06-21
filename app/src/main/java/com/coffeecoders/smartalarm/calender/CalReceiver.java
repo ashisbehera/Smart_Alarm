e2a0b4bf72bad781cc081ebd97d0fc5b2b973747
Ashis Behera <ashisb814@gmail.com>
2022-06-21 23:53:14 +0530

    receiver added for background check of cal events

---
+++ app/src/main/java/com/coffeecoders/smartalarm/calender/CalReceiver.java
@@ -0,0 +1,12 @@
+package com.coffeecoders.smartalarm.calender;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CalReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
